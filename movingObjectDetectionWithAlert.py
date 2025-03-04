0dbf4c6895d845c8c387b37bfc707ec2122fc8cf
arivanan0218 <arivanan_v_e23@engug.ruh.ac.lk>
2025-03-04 10:16:06 +0530

    Finished moving object ditection with alert

---
+++ movingObjectDetectionWithAlert.py
@@ -0,0 +1,67 @@
+import cv2
+import imutils
+import pygame
+
+pygame.mixer.init()
+alert_sound = "alert.mp3"
+pygame.mixer.music.load(alert_sound)
+
+cam = cv2.VideoCapture(0)
+
+firstFrame = None
+area = 500
+alarm_playing = False
+
+while True:
+    _,img = cam.read()
+    text = "Normal"
+
+    img = imutils.resize(img, width=1000)
+
+    grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    gaussianImg = cv2.GaussianBlur(grayImg, (21,21), 0)
+
+    if firstFrame is None:
+            firstFrame = gaussianImg
+            continue
+
+    imgDiff = cv2.absdiff(firstFrame, gaussianImg)
+    threshImg = cv2.threshold(imgDiff, 25, 255, cv2.THRESH_BINARY)[1]
+    threshImg = cv2.dilate(threshImg, None, iterations=2)
+
+    cnts = cv2.findContours(threshImg.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+
+    object_detected = False
+    for c in cnts:
+            if cv2.contourArea(c) < area:
+                    continue
+            (x, y, w, h) = cv2.boundingRect(c)
+            cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+            text = "Moving object detected"
+            object_detected = True
+    
+    if object_detected and not alarm_playing:
+            pygame.mixer.music.play()
+            alarm_playing = True
+
+
+    if not object_detected and alarm_playing:
+            pygame.mixer.music.stop()
+            alarm_playing = False
+        
+        
+    print(text)
+    cv2.putText(img, text, (10,20),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    cv2.imshow("camaraFeed", img)
+
+    key = cv2.waitKey(30)
+    print(key)
+    if key == ord("q"):
+        pygame.mixer.music.stop()
+        break
+
+cam.release()
+cv2.destroyAllWindows()
